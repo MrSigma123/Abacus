9c4c6249f0f3892157136f5bcb88e069d3c39fce
MrSigma123 <karol.czolpinski@gmail.com>
2024-07-04 23:11:46 +0200

    Write program design overview

---
+++ abacus.c
@@ -0,0 +1,8 @@
+/* Program design:
+  Enter n-based positional nymeral system
+  // create colorful pattern for certain place value representation
+  Two types of input:
+    - enter the value
+    - use wasd to maneuver the abacus and move the rings (stars) to
+    change the value
+*/
